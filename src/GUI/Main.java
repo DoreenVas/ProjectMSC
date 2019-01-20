c908f863aa8df2f9916273a235d34dbd3febbf5b
Doreen <Doreen@DESKTOP-S59AJ7A>
2019-01-20 17:18:23 +0200

    doreen. connecting screens.

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -6,25 +6,42 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import javax.swing.*;
 import java.awt.*;
 
 public class Main extends Application {
     // members
     private double window_height;
     private double window_width;
+    private JDialog dialog = new JDialog();
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
         primaryStage.setTitle("MSC");
         // get the size of the screen
-        Rectangle window = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();
-        this.window_height = window.height;
-        this.window_width = window.width;
+        //Rectangle window = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();
+        //this.window_height = window.height;
+        //this.window_width = window.width;
         // set the window size
-        Scene main_window = new Scene(root,  this.window_width - 5,  this.window_height- 15);
+        setWindowSize();
+        Scene main_window = new Scene(root,  this.window_width,  this.window_height);
         primaryStage.setScene(main_window);
+        primaryStage.setResizable(false);
         primaryStage.show();
+    }
+
+    private void setWindowSize(){
+        //size of the screen
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+        //height of the task bar
+        Insets scnMax = Toolkit.getDefaultToolkit().getScreenInsets(dialog.getGraphicsConfiguration());
+        int taskBarSize = scnMax.bottom;
+
+        //available size of the screen
+        this.window_width = screenSize.width - dialog.getWidth();
+        this.window_height = screenSize.height - taskBarSize - dialog.getHeight();
     }
 
     public static void main(String[] args) {
--- src/GUI/MainWindow.java
+++ src/GUI/MainWindow.java
@@ -1,5 +1,77 @@
 package GUI;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.awt.*;
 
 public class MainWindow {
+    @FXML
+    private Button start;
+    @FXML
+    private Button instructions;
+    @FXML
+    private Button exit;
 
+    private double window_height;
+    private double window_width;
+
+    @FXML
+    protected void instructions() {
+        try {
+            Stage stage = (Stage) instructions.getScene().getWindow();
+            AnchorPane root = FXMLLoader.load(getClass().getResource("InstructionsWindow.fxml"));
+            stage.setTitle("Instructions");
+            // get the size of the screen
+            Rectangle window = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();
+            this.window_height = window.height;
+            this.window_width = window.width;
+            // set the window size
+            Scene scene = new Scene(root,  this.window_width - 5,  this.window_height- 15);
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.setResizable(false);
+            stage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /***
+     * Loading the Search window when the "start" button is clicked.
+     */
+    @FXML
+    protected void start() {
+        try {
+            Stage stage = (Stage) start.getScene().getWindow();
+            AnchorPane root = FXMLLoader.load(getClass().getResource("GameWindow.fxml"));
+            stage.setTitle("Game");
+            // get the size of the screen
+            Rectangle window = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();
+            this.window_height = window.height;
+            this.window_width = window.width;
+            // set the window size
+            Scene scene = new Scene(root,  this.window_width - 5,  this.window_height- 15);
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.setResizable(false);
+            stage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /***
+     * Exiting the application.
+     */
+    @FXML
+    protected void exit() {
+        Stage stage = (Stage) exit.getScene().getWindow();
+        stage.close();
+    }
 }
