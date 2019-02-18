bda1ed0d11b9d30e046d372c96facfc89d640d21
NadavSpitzer <NadavSpitzer@NADAV-PC>
2019-02-18 14:55:32 +0200

    Nadav's commit.

--- src/GUI/InstructionsWindow.java
+++ src/GUI/InstructionsWindow.java
@@ -6,7 +6,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-
 import java.awt.*;
 
 public class InstructionsWindow {
--- src/GUI/SignUpWindow.java
+++ src/GUI/SignUpWindow.java
@@ -1,21 +1,42 @@
 package GUI;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class SignUpWindow {
+public class SignUpWindow implements Initializable{
 
     @FXML
     private Button back;
 
     private double window_height;
     private double window_width;
+
+    @FXML
+    private ComboBox<String> gender;
+
+    @FXML
+    private ComboBox<String> dominantHand;
+
+    private ObservableList<String> genderSet = FXCollections.observableArrayList("נקבה","זכר");
+    private ObservableList<String> handSet = FXCollections.observableArrayList("שמאל","ימין");
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        gender.setItems(genderSet);
+        dominantHand.setItems(handSet);
+    }
 
     @FXML
     protected void mainWindow() {
